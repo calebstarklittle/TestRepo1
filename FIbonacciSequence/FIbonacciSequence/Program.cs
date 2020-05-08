fc6d2dd236fc96198fc7a03a6bf31000e89265b2
Caleb Stark <calebstarklittle@gmail.com>
2020-05-08 15:31:31 -0400

    Fibonacci Sequence Added

---
+++ FIbonacciSequence/FIbonacciSequence/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace FibonacciSequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 0, num2 = 1;
+            int i;
+
+            int n = 15;
+
+            Console.WriteLine("Here are the Fibonacci Numbers: ");
+            Console.Write(num1 + " " + num2 + " ");
+
+            for (i = 2; i < n; i++)
+            {
+                int num3 = num1 + num2;
+                Console.Write(num3 + " ");
+                num1 = num2;
+                num2 = num3;
+            }
+
+        }
+    }
+}
